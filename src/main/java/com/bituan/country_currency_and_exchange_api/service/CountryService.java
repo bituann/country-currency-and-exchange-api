b132ac1361b3193aade5ae6f0df70f84973e7b4c
bituan <113036994+bituann@users.noreply.github.com>
2025-10-25 13:26:08 +0100

    Inject country repository into country service

--- src/main/java/com/bituan/country_currency_and_exchange_api/service/CountryService.java
+++ src/main/java/com/bituan/country_currency_and_exchange_api/service/CountryService.java
@@ -1,5 +1,15 @@
 package com.bituan.country_currency_and_exchange_api.service;
 
+import com.bituan.country_currency_and_exchange_api.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class CountryService {
+
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryService (CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
 
 }
