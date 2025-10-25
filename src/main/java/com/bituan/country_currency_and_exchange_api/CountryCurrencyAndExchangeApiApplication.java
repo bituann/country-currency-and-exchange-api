56bcb0477a274894b0416d376fb7c4dc7d480baf
bituan <113036994+bituann@users.noreply.github.com>
2025-10-25 11:46:53 +0100

    Initial setup

---
+++ src/main/java/com/bituan/country_currency_and_exchange_api/CountryCurrencyAndExchangeApiApplication.java
@@ -0,0 +1,13 @@
+package com.bituan.country_currency_and_exchange_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCurrencyAndExchangeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryCurrencyAndExchangeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bituan/country_currency_and_exchange_api/CountryCurrencyAndExchangeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bituan.country_currency_and_exchange_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryCurrencyAndExchangeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
