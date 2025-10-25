b680743cddeaeb03652dd462eed8d857d5e3112f
bituan <113036994+bituann@users.noreply.github.com>
2025-10-25 13:16:54 +0100

    Add repository for DB

---
+++ src/main/java/com/bituan/country_currency_and_exchange_api/repository/CountryRepository.java
@@ -0,0 +1,7 @@
+package com.bituan.country_currency_and_exchange_api.repository;
+
+import com.bituan.country_currency_and_exchange_api.entity.CountryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<CountryEntity, Long> {
+}
