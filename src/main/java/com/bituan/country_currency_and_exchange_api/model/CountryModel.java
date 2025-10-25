257c066c08da5f6eb9ac6b23efbfe5357f22a154
bituan <113036994+bituann@users.noreply.github.com>
2025-10-25 13:03:44 +0100

    Complete country model

--- src/main/java/com/bituan/country_currency_and_exchange_api/model/CountryModel.java
+++ src/main/java/com/bituan/country_currency_and_exchange_api/model/CountryModel.java
@@ -1,4 +1,98 @@
 package com.bituan.country_currency_and_exchange_api.model;
 
+import java.time.Instant;
+
 public class CountryModel {
+    private Long id;
+    private String name;
+    private String capital;
+    private String region;
+    private Long population;
+    private String currencyCode;
+    private Long exchangeRate;
+    private Long estimatedGdp;
+    private String flagUrl;
+    private Instant lastRefreshedAt;
+
+    public CountryModel() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public Long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Long population) {
+        this.population = population;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public Long getExchangeRate() {
+        return exchangeRate;
+    }
+
+    public void setExchangeRate(Long exchangeRate) {
+        this.exchangeRate = exchangeRate;
+    }
+
+    public Long getEstimatedGdp() {
+        return estimatedGdp;
+    }
+
+    public void setEstimatedGdp(Long estimatedGdp) {
+        this.estimatedGdp = estimatedGdp;
+    }
+
+    public String getFlagUrl() {
+        return flagUrl;
+    }
+
+    public void setFlagUrl(String flagUrl) {
+        this.flagUrl = flagUrl;
+    }
+
+    public Instant getLastRefreshedAt() {
+        return lastRefreshedAt;
+    }
+
+    public void setLastRefreshedAt(Instant lastRefreshedAt) {
+        this.lastRefreshedAt = lastRefreshedAt;
+    }
 }
