d16a509b3e2ea50d9127b28f372fa376262d443b
bituan <113036994+bituann@users.noreply.github.com>
2025-10-25 13:45:14 +0100

    Add query method to get country by name

--- src/main/java/com/bituan/country_currency_and_exchange_api/repository/CountryRepository.java
+++ src/main/java/com/bituan/country_currency_and_exchange_api/repository/CountryRepository.java
@@ -3,5 +3,8 @@ package com.bituan.country_currency_and_exchange_api.repository;
 import com.bituan.country_currency_and_exchange_api.entity.CountryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CountryRepository extends JpaRepository<CountryEntity, Long> {
+    List<CountryEntity> findByName (String name);
 }
