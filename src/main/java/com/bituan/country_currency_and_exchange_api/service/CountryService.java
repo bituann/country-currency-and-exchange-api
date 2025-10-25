60dc7766a9c755c61ff122d8463df2a81f368c8c
bituan <113036994+bituann@users.noreply.github.com>
2025-10-25 13:28:34 +0100

    Add method to get all countries

--- src/main/java/com/bituan/country_currency_and_exchange_api/service/CountryService.java
+++ src/main/java/com/bituan/country_currency_and_exchange_api/service/CountryService.java
@@ -1,15 +1,22 @@
 package com.bituan.country_currency_and_exchange_api.service;
 
+import com.bituan.country_currency_and_exchange_api.entity.CountryEntity;
+import com.bituan.country_currency_and_exchange_api.model.CountryModel;
 import com.bituan.country_currency_and_exchange_api.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 public class CountryService {
 
-    private CountryRepository countryRepository;
+    private final CountryRepository countryRepository;
 
     @Autowired
     public CountryService (CountryRepository countryRepository) {
         this.countryRepository = countryRepository;
     }
 
+    public List<CountryEntity> getAllCountries () {
+        return countryRepository.findAll();
+    }
 }
