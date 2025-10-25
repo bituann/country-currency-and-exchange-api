542a2a181df98beac391dda7c7b3694ee9357822
bituan <113036994+bituann@users.noreply.github.com>
2025-10-25 16:07:42 +0100

    Rename inner class to prevent conflict

--- src/main/java/com/bituan/country_currency_and_exchange_api/service/RestCountriesApiService.java
+++ src/main/java/com/bituan/country_currency_and_exchange_api/service/RestCountriesApiService.java
@@ -2,7 +2,6 @@ package com.bituan.country_currency_and_exchange_api.service;
 
 import com.bituan.country_currency_and_exchange_api.model.CountryModel;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,7 +14,7 @@ public class RestCountriesApiService {
         RestTemplate restTemplate = new RestTemplate();
         String restCountriesUrl = "https://restcountries.com/v2/all?fields=name,capital,region,population,flag,currencies";
 
-        ResponseEntity<Response> response = restTemplate.getForEntity(restCountriesUrl, Response.class);
+        ResponseEntity<RCResponse> response = restTemplate.getForEntity(restCountriesUrl, RCResponse.class);
 
         if (response.getStatusCode() == HttpStatus.OK) {
             return response.getBody().getResponse();
@@ -25,7 +24,7 @@ public class RestCountriesApiService {
     }
 }
 
-class Response {
+class RCResponse {
     private List<CountryModel> response;
 
     public List<CountryModel> getResponse() {
