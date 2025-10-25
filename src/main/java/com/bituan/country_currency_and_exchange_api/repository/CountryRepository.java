c628289573fc05bdea759054362a6b4bf2f8df40
bituan <113036994+bituann@users.noreply.github.com>
2025-10-25 13:49:53 +0100

    Change return type of query method

--- src/main/java/com/bituan/country_currency_and_exchange_api/repository/CountryRepository.java
+++ src/main/java/com/bituan/country_currency_and_exchange_api/repository/CountryRepository.java
@@ -3,8 +3,6 @@ package com.bituan.country_currency_and_exchange_api.repository;
 import com.bituan.country_currency_and_exchange_api.entity.CountryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CountryRepository extends JpaRepository<CountryEntity, Long> {
-    List<CountryEntity> findByName (String name);
+    CountryEntity findByName (String name);
 }
