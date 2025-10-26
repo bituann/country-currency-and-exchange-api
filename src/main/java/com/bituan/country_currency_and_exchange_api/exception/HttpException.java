513711544a3d25f2de427c8765b4a2782c051dda
bituan <113036994+bituann@users.noreply.github.com>
2025-10-26 19:19:59 +0100

    Add custom exception class for HTTP exceptions

--- src/main/java/com/bituan/country_currency_and_exchange_api/exception/HttpException.java
+++ src/main/java/com/bituan/country_currency_and_exchange_api/exception/HttpException.java
@@ -1,7 +1,22 @@
 package com.bituan.country_currency_and_exchange_api.exception;
 
+import com.bituan.country_currency_and_exchange_api.model.HttpExceptionMessageModel;
+import org.springframework.http.HttpStatus;
+
 public class HttpException extends Exception{
-    public HttpException (Throwable cause) {
-        super(cause);
+    private HttpStatus status;
+    private HttpExceptionMessageModel customMessage;
+
+    public HttpException (HttpStatus status, String error, String details) {
+        this.status = status;
+        this.customMessage = new HttpExceptionMessageModel(error, details);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public HttpExceptionMessageModel getCustomMessage() {
+        return customMessage;
     }
 }
