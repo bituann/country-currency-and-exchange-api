2ae8b9371c8b7fe1f491411d108493ea2b13b648
bituan <113036994+bituann@users.noreply.github.com>
2025-10-26 18:19:58 +0100

    Make exchange rate response model an outer class

--- src/main/java/com/bituan/country_currency_and_exchange_api/model/ErResponseModel.java
+++ src/main/java/com/bituan/country_currency_and_exchange_api/model/ErResponseModel.java
@@ -1,4 +1,18 @@
 package com.bituan.country_currency_and_exchange_api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ErResponseModel {
+    private Map<String, Integer> rates;
+
+    public Map<String, Integer> getRates() {
+        return rates;
+    }
+
+    public void setRates(Map<String, Integer> rates) {
+        this.rates = rates;
+    }
 }
