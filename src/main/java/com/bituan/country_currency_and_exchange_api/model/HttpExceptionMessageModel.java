963fd72a2e8d2b6fdd3e044756132b0802d5618f
bituan <113036994+bituann@users.noreply.github.com>
2025-10-26 18:47:54 +0100

    Add message model for http exception

---
+++ src/main/java/com/bituan/country_currency_and_exchange_api/model/HttpExceptionMessageModel.java
@@ -0,0 +1,32 @@
+package com.bituan.country_currency_and_exchange_api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class HttpExceptionMessageModel {
+    private String error;
+    private String details;
+
+    public HttpExceptionMessageModel () {}
+
+    public HttpExceptionMessageModel(String error, String details) {
+        this.error = error;
+        this.details = details;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
