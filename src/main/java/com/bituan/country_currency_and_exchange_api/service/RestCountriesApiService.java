1ccac7a86af72e2fd00280b7e35f1821677a6829
bituan <113036994+bituann@users.noreply.github.com>
2025-10-26 18:20:47 +0100

    Use external response class

--- src/main/java/com/bituan/country_currency_and_exchange_api/service/RestCountriesApiService.java
+++ src/main/java/com/bituan/country_currency_and_exchange_api/service/RestCountriesApiService.java
@@ -1,6 +1,7 @@
 package com.bituan.country_currency_and_exchange_api.service;
 
 import com.bituan.country_currency_and_exchange_api.model.CountryModel;
+import com.bituan.country_currency_and_exchange_api.model.RcResponseModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,24 +15,12 @@ public class RestCountriesApiService {
         RestTemplate restTemplate = new RestTemplate();
         String restCountriesUrl = "https://restcountries.com/v2/all?fields=name,capital,region,population,flag,currencies";
 
-        ResponseEntity<RCResponse> response = restTemplate.getForEntity(restCountriesUrl, RCResponse.class);
+        ResponseEntity<RcResponseModel> response = restTemplate.getForEntity(restCountriesUrl, RcResponseModel.class);
 
         if (response.getStatusCode() == HttpStatus.OK) {
             return response.getBody().getResponse();
         }
 
         return null;
-    }
-}
-
-class RCResponse {
-    private List<CountryModel> response;
-
-    public List<CountryModel> getResponse() {
-        return response;
-    }
-
-    public void setResponse(List<CountryModel> response) {
-        this.response = response;
     }
 }
