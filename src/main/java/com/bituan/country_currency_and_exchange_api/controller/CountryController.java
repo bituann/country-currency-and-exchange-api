81c67b76aaf8e95ef409f67ab28670dbeeaa4131
bituan <113036994+bituann@users.noreply.github.com>
2025-10-26 12:47:22 +0100

    Inject services into controller

--- src/main/java/com/bituan/country_currency_and_exchange_api/controller/CountryController.java
+++ src/main/java/com/bituan/country_currency_and_exchange_api/controller/CountryController.java
@@ -1,8 +1,27 @@
 package com.bituan.country_currency_and_exchange_api.controller;
 
+import com.bituan.country_currency_and_exchange_api.service.CountryService;
+import com.bituan.country_currency_and_exchange_api.service.ExchangeRateAPIService;
+import com.bituan.country_currency_and_exchange_api.service.RestCountriesApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CountryController {
+
+    private final CountryService countryService;
+    private final ExchangeRateAPIService exchangeRateAPIService;
+    private final RestCountriesApiService restCountriesApiService;
+
+    @Autowired
+    public CountryController (CountryService countryService, ExchangeRateAPIService exchangeRateAPIService,
+                       RestCountriesApiService restCountriesApiService) {
+        this.countryService = countryService;
+        this.exchangeRateAPIService = exchangeRateAPIService;
+        this.restCountriesApiService = restCountriesApiService;
+    }
+
 
 }
