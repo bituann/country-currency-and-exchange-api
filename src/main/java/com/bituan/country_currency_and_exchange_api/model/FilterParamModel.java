f3008a91d98c51ce931273e6e1688c0690843113
bituan <113036994+bituann@users.noreply.github.com>
2025-10-27 00:09:36 +0100

    Add model for filter params

---
+++ src/main/java/com/bituan/country_currency_and_exchange_api/model/FilterParamModel.java
@@ -0,0 +1,42 @@
+package com.bituan.country_currency_and_exchange_api.model;
+
+public class FilterParamModel {
+    private String region;
+    private String currency;
+    private Long gdp;
+    private String sortBy;
+
+    public FilterParamModel() {}
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Long getGdp() {
+        return gdp;
+    }
+
+    public void setGdp(Long gdp) {
+        this.gdp = gdp;
+    }
+
+    public String getSortBy() {
+        return sortBy;
+    }
+
+    public void setSortBy(String sortBy) {
+        this.sortBy = sortBy;
+    }
+}
