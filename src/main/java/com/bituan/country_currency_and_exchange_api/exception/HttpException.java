b5d5c873070723e95e85239b2554152a3c929ddd
bituan <113036994+bituann@users.noreply.github.com>
2025-10-26 18:43:28 +0100

    Add main general HTTP exception

---
+++ src/main/java/com/bituan/country_currency_and_exchange_api/exception/HttpException.java
@@ -0,0 +1,7 @@
+package com.bituan.country_currency_and_exchange_api.exception;
+
+public class HttpException extends Exception{
+    public HttpException (Throwable cause) {
+        super(cause);
+    }
+}
