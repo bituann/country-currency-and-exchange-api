44b8d1bc3592b2fa2624ab5eb700bdf3fcea3b7b
bituan <113036994+bituann@users.noreply.github.com>
2025-10-26 21:37:58 +0100

    Handle errors and exceptions

--- src/main/java/com/bituan/country_currency_and_exchange_api/service/RestCountriesApiService.java
+++ src/main/java/com/bituan/country_currency_and_exchange_api/service/RestCountriesApiService.java
@@ -2,7 +2,8 @@ package com.bituan.country_currency_and_exchange_api.service;
 
 import com.bituan.country_currency_and_exchange_api.exception.HttpException;
 import com.bituan.country_currency_and_exchange_api.model.CountryModel;
-import com.bituan.country_currency_and_exchange_api.model.RcResponseModel;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -16,10 +17,14 @@ public class RestCountriesApiService {
         RestTemplate restTemplate = new RestTemplate();
         String restCountriesUrl = "https://restcountries.com/v2/all?fields=name,capital,region,population,flag,currencies";
 
-        ResponseEntity<RcResponseModel> response = restTemplate.getForEntity(restCountriesUrl, RcResponseModel.class);
+        try {
+            ResponseEntity<List<CountryModel>> response = restTemplate.exchange(restCountriesUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<CountryModel>>() {});
 
-        if (response.getStatusCode() == HttpStatus.OK) {
-            return response.getBody().getResponse();
+            if (response.getStatusCode() == HttpStatus.OK) {
+                return response.getBody();
+            }
+        } catch (Exception e) {
+            throw new HttpException(HttpStatus.valueOf(503), "External data source unavailable", "Could not fetch data from restcountries");
         }
 
         throw new HttpException(HttpStatus.valueOf(503), "External data source unavailable", "Could not fetch data from restcountries");
