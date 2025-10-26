c58816b7999284563e48d4c4ec12087472d78465
bituan <113036994+bituann@users.noreply.github.com>
2025-10-26 18:27:01 +0100

    Make rest countries response model an outer class

--- src/main/java/com/bituan/country_currency_and_exchange_api/model/RcResponseModel.java
+++ src/main/java/com/bituan/country_currency_and_exchange_api/model/RcResponseModel.java
@@ -1,4 +1,15 @@
 package com.bituan.country_currency_and_exchange_api.model;
 
+import java.util.List;
+
 public class RcResponseModel {
+    private List<CountryModel> response;
+
+    public List<CountryModel> getResponse() {
+        return response;
+    }
+
+    public void setResponse(List<CountryModel> response) {
+        this.response = response;
+    }
 }
