01dc1c7aae77e64b156593b29bd66678eff11691
bituan <113036994+bituann@users.noreply.github.com>
2025-10-26 19:21:38 +0100

    Add global handler for custom HTTP exception

---
+++ src/main/java/com/bituan/country_currency_and_exchange_api/exception/HttpExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.bituan.country_currency_and_exchange_api.exception;
+
+import com.bituan.country_currency_and_exchange_api.model.HttpExceptionMessageModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class HttpExceptionHandler {
+    @ExceptionHandler(HttpException.class)
+    public ResponseEntity<HttpExceptionMessageModel> handleException (HttpException exception) {
+        return ResponseEntity.status(exception.getStatus()).body(exception.getCustomMessage());
+    }
+}
