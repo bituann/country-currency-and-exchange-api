b87e37fed70b8acb3931a0673e2cbaf473312853
bituan <113036994+bituann@users.noreply.github.com>
2025-10-27 00:42:01 +0100

    Add method to get all countries sorted by gdp

--- src/main/java/com/bituan/country_currency_and_exchange_api/repository/CountryRepository.java
+++ src/main/java/com/bituan/country_currency_and_exchange_api/repository/CountryRepository.java
@@ -3,9 +3,13 @@ package com.bituan.country_currency_and_exchange_api.repository;
 import com.bituan.country_currency_and_exchange_api.entity.CountryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CountryRepository extends JpaRepository<CountryEntity, Long> {
     CountryEntity findByName (String name);
     void deleteByName (String name);
     boolean existsByName (String name);
     CountryEntity findFirstBy();
+    List<CountryEntity> findAllOrderByGdpDesc();
+    List<CountryEntity> findAllOrderByGdpAsc();
 }
