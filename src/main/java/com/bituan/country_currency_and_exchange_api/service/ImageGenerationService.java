fd1ae14df813b84ebc3534a3f73e153bb1cd2b50
bituan <113036994+bituann@users.noreply.github.com>
2025-10-27 11:14:53 +0100

    Update image generation service

--- src/main/java/com/bituan/country_currency_and_exchange_api/service/ImageGenerationService.java
+++ src/main/java/com/bituan/country_currency_and_exchange_api/service/ImageGenerationService.java
@@ -14,21 +14,26 @@ public class ImageGenerationService {
     @Autowired
     public ImageGenerationService() {}
 
-    public void generateImage (String string) {
-        int width = 400;
-        int height = 200;
+    public void generateImage (String string, String name) {
+        int width = 800;
+        int height = 400;
 
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics2D graphics2D = image.createGraphics();
 
-        graphics2D.setBackground(Color.WHITE);
+        graphics2D.setColor(Color.WHITE);
+        graphics2D.fillRect(0, 0, width, height);
+
+        Font font = new Font("Arial", Font.BOLD, 30);
+        graphics2D.setFont(font);
+
         graphics2D.setColor(Color.BLACK);
-        graphics2D.drawString(string, 100, 100);
+        graphics2D.drawString(string, 400, 200);
 
         graphics2D.dispose();
 
         try {
-            ImageIO.write(image, "PNG", new File("generated_image.png"));
+            ImageIO.write(image, "PNG", new File("%s.png".formatted(name)));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
